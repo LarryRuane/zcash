5b3817ce62f379e8171307d00dc162cd4ae05811
Jay Graber <jay@z.cash>
2018-08-28 17:16:04 -0700

    Add rpc test for sprout txs z_listunspent

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -24,6 +24,7 @@ testScripts=(
     'wallet_nullifiers.py'
     'wallet_1941.py'
     'wallet_addresses.py'
+    'wallet_listnotes.py'
     'listtransactions.py'
     'mempool_resurrect_test.py'
     'txn_doublespend.py'
